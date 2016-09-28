c823ceee80c5009986e9a9e61e4f0dca355440e6
conykais <838437017@qq.com>
2016-09-28 18:07:52 +0800

    新增下载提示    Notification

--- Utils/app/src/main/java/com/cqg/exmple/activity/MainActivity.java
+++ Utils/app/src/main/java/com/cqg/exmple/activity/MainActivity.java
@@ -1,11 +1,15 @@
 package com.cqg.exmple.activity;
 
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.RemoteViews;
 
 import com.cqg.exmple.R;
 import com.cqg.exmple.comm.WebAPI;
@@ -27,11 +31,16 @@ import java.io.InputStreamReader;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private Notification mNotification;
+    private NotificationManager manager;
+    private RemoteViews views;
+    private int p = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EventBus.getDefault().register(this);
+
     }
 
     public void update(View view) {
@@ -94,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 }).setNegativeButton("我不", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                dialog.dismiss();
             }
         });
 
@@ -103,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onEventBackgroundThread(Version version){
+    public void onEventBackgroundThread(final Version version){
 
         String path = SdCardHelper.getSdCardPath() +"/APK";
         DownloadHelper.downloadFile(version.getDownloadURL(), path, "mukawang.apk", new DownloadHelper.DownloadCallBack() {
@@ -133,10 +142,51 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void progress(int progress, int total) {
-                Log.i (TAG, "progress: ");
+            public void progress(final int progress, int total) {
+
+
+                final int progre = (int)((progress / (total *1.0f))*100);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        if (mNotification == null) {
+                            Notification.Builder builder = new Notification.Builder(MainActivity.this);
+                            builder.setContentTitle("更新中");
+                            builder.setTicker("正在下载新版本...");
+                            builder.setSmallIcon(R.mipmap.ic_launcher);
+                            mNotification = builder.build();
+                            mNotification.flags = Notification.FLAG_AUTO_CANCEL;
+                            views = new RemoteViews(getPackageName(),R.layout.download_apk);
+                            mNotification.contentView = views;
+
+                            views.setProgressBar(R.id.progressBar,100,progre,false);
+
+                            manager = (NotificationManager) MainActivity.this.getSystemService(Context.NOTIFICATION_SERVICE);
+                        }
+
+                        if (p!=progre){
+                            p = progre;
+                            views.setProgressBar(R.id.progressBar,100,progre,false);
+                            if (p ==100){
+                                views.setViewVisibility(R.id.progressBar, View.GONE);
+                                views.setViewVisibility(R.id.done,View.VISIBLE);
+                                Log.i(TAG, "run: 下载完啦 ....");
+                            }
+                            Log.i(TAG, "progress: " + progre);
+                            manager.notify(R.id.progressBar,mNotification);
+                        }
+
+                    }
+                });
+
+
+
             }
         });
+
+
+
     }
 
 }
